acb119e5a4575e76b6cc5063878e5e5e1f081c0b
Thomas Zamojski <tzamojski@gaia.quadratic-labs.com>
2023-01-18 09:56:39 +0000

    Add poseidon precompile for starkware's variant
    Poseidon precompile is added to the list of precompiles. It supports a single variant for the moment.
    
    The precompile statically links poseidon-rs library to perform hashing. The same tests are used in core/vm/contracts_test.go as in poseidon-rs tests.

---
+++ crypto/poseidon/poseidon.go
@@ -0,0 +1,19 @@
+package poseidon
+
+/*
+#cgo CFLAGS: -I.
+#cgo LDFLAGS: -L. -l:libposeidon.a
+#include "poseidon.h"
+*/
+import "C"
+
+import (
+	"unsafe"
+)
+
+
+func Hash(input []byte) ([]byte) {
+    output := make([]byte, 64)
+    count := C.c_hash_sw2((*C.uint8_t)(unsafe.Pointer(&input[0])), C.size_t(len(input)), (*C.uint8_t)(unsafe.Pointer(&output[0])), C.size_t(len(output)))
+    return output[:count]
+}
